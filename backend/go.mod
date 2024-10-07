c281e7ec4b9ad1884e732b3639c22dbd3a8b1a1d
Bungo <alxber-0@student.ltu.se>
2024-10-07 09:13:07 +0200

    added mqtt subscriber

--- backend/go.mod
+++ backend/go.mod
@@ -6,9 +6,13 @@ require (
 )
 
 require (
+	github.com/eclipse/paho.mqtt.golang v1.5.0 // indirect
+	github.com/gorilla/websocket v1.5.3 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	golang.org/x/crypto v0.27.0 // indirect
+	golang.org/x/net v0.27.0 // indirect
+	golang.org/x/sync v0.8.0 // indirect
 	golang.org/x/text v0.18.0 // indirect
 )
 
---
+++ backend/mqtt/mqtt_subscriber.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+)
+
+var messagePubHandler mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Message) {
+	fmt.Printf("Received message: %s from topic: %s\n", msg.Payload(), msg.Topic())
+}
+
+var connectHandler mqtt.OnConnectHandler = func(client mqtt.Client) {
+	fmt.Println("Connected to MQTT broker")
+}
+
+var connectLostHandler mqtt.ConnectionLostHandler = func(client mqtt.Client, err error) {
+	fmt.Printf("Connection lost: %v\n", err)
+}
+
+func main() {
+	// Set MQTT broker URL
+	broker := "tcp://broker.hivemq.com:1883" // HiveMQ public broker
+	topic := "test/topic"
+
+	opts := mqtt.NewClientOptions()
+	opts.AddBroker(broker)
+	opts.SetClientID("go_mqtt_subscriber")
+
+	// If we set up authentication on the broker
+	// opts.SetUsername("your_username")
+	// opts.SetPassword("your_password")
+
+	opts.SetDefaultPublishHandler(messagePubHandler)
+	opts.OnConnect = connectHandler
+	opts.OnConnectionLost = connectLostHandler
+
+	// Create client
+	client := mqtt.NewClient(opts)
+	if token := client.Connect(); token.Wait() && token.Error() != nil {
+		log.Fatalf("Error connecting to broker: %v", token.Error())
+		os.Exit(1)
+	}
+
+	// Subscribe to the topic
+	if token := client.Subscribe(topic, 1, nil); token.Wait() && token.Error() != nil {
+		log.Fatalf("Error subscribing to topic: %v", token.Error())
+		os.Exit(1)
+	}
+
+	// Keep the subscriber running
+	for {
+		time.Sleep(1 * time.Second)
+	}
+
+	// Disconnect when done
+	// client.Disconnect(250)
+}
