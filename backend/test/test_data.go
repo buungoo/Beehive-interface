69fed19dd236c911760e4ae6f16d57141fc0d844
linseb-9 <sli0202@hotmail.com>
2024-10-31 12:19:22 +0000

    Added test data, not working yet

--- backend/test/test_data.go
+++ backend/test/test_data.go
@@ -1,18 +1,27 @@
 package test
 
 import (
+    "beehive_api/models"
 	"context"
+    //"encoding/json"
 	"fmt"
 	"github.com/jackc/pgx/v5/pgxpool"
-	"beehive_api/models"
-    "time"
-    "log"
     "golang.org/x/crypto/bcrypt"
+    //"os"
+    "log"
+    "time"
 )
 
 // Inject harmful data into the database for testing
 
-func InjectTestData(dbpool *pgxpool.Pool) error {
+func InjectTestData(dbPool *pgxpool.Pool) error {
+
+    // Acuire connection from the connection pool
+	conn, err := dbPool.Acquire(context.Background())
+	if err!=nil {
+	 log.Fatal("Error while acquiring connection from the database pool!!")
+	} 
+	defer conn.Release()
 
     // Hash password with bcrypt
 	password1, err := bcrypt.GenerateFromPassword([]byte("pass1"), bcrypt.DefaultCost)
@@ -25,64 +34,91 @@ func InjectTestData(dbpool *pgxpool.Pool) error {
 		log.Fatal("Error hashing password:", err)
 	}
 
-    
 
     // Insert test data into the users table
     var user1ID, user2ID int
-    err = dbpool.QueryRow(context.Background(), "INSERT INTO users (username, password) VALUES ($1, $2) RETURNING id", "user1", password1).Scan(&user1ID)
+    err = conn.QueryRow(context.Background(), "INSERT INTO users (username, password) VALUES ($1, $2) RETURNING id", "user1", password1).Scan(&user1ID)
     if err != nil {
         return fmt.Errorf("failed to insert user1: %v", err)
     }
-    err = dbpool.QueryRow(context.Background(), "INSERT INTO users (username, password) VALUES ($1, $2) RETURNING id", "user2", password2).Scan(&user2ID)
+    err = conn.QueryRow(context.Background(), "INSERT INTO users (username, password) VALUES ($1, $2) RETURNING id", "user2", password2).Scan(&user2ID)
     if err != nil {
         return fmt.Errorf("failed to insert user2: %v", err)
     }
 
     // Insert test data into the beehives table
     var beehive1ID, beehive2ID int
-    err = dbpool.QueryRow(context.Background(), "INSERT INTO beehives (name, user_id) VALUES ($1, $2) RETURNING id", "Beehive A", user1ID).Scan(&beehive1ID)
+    err = conn.QueryRow(context.Background(), "INSERT INTO beehives (name, user_id) VALUES ($1, $2) RETURNING id", "Beehive A", user1ID).Scan(&beehive1ID)
     if err != nil {
         return fmt.Errorf("failed to insert beehive1: %v", err)
     }
-    err = dbpool.QueryRow(context.Background(), "INSERT INTO beehives (name, user_id) VALUES ($1, $2) RETURNING id", "Beehive B", user2ID).Scan(&beehive2ID)
+    err = conn.QueryRow(context.Background(), "INSERT INTO beehives (name, user_id) VALUES ($1, $2) RETURNING id", "Beehive B", user2ID).Scan(&beehive2ID)
     if err != nil {
         return fmt.Errorf("failed to insert beehive2: %v", err)
     }
 
     // Insert test data into the sensors table
     var sensor1ID, sensor2ID, sensor3ID, sensor4ID, sensor5ID, sensor6ID, sensor7ID, sensor8ID int
-    err = dbpool.QueryRow(context.Background(), "INSERT INTO sensors (type, beehive_id) VALUES ($1, $2) RETURNING id", "temperature", beehive1ID).Scan(&sensor1ID)
+    err = conn.QueryRow(context.Background(), "INSERT INTO sensors (type, beehive_id) VALUES ($1, $2) RETURNING id", "temperature", beehive1ID).Scan(&sensor1ID)
     if err != nil {
         return fmt.Errorf("failed to insert sensor1: %v", err)
     }
-    err = dbpool.QueryRow(context.Background(), "INSERT INTO sensors (type, beehive_id) VALUES ($1, $2) RETURNING id", "humidity", beehive1ID).Scan(&sensor2ID)
+    err = conn.QueryRow(context.Background(), "INSERT INTO sensors (type, beehive_id) VALUES ($1, $2) RETURNING id", "humidity", beehive1ID).Scan(&sensor2ID)
     if err != nil {
         return fmt.Errorf("failed to insert sensor2: %v", err)
     }
-    err = dbpool.QueryRow(context.Background(), "INSERT INTO sensors (type, beehive_id) VALUES ($1, $2) RETURNING id", "weight", beehive1ID).Scan(&sensor3ID)
+    err = conn.QueryRow(context.Background(), "INSERT INTO sensors (type, beehive_id) VALUES ($1, $2) RETURNING id", "weight", beehive1ID).Scan(&sensor3ID)
     if err != nil {
         return fmt.Errorf("failed to insert sensor3: %v", err)
     }
-	err = dbpool.QueryRow(context.Background(), "INSERT INTO sensors (type, beehive_id) VALUES ($1, $2) RETURNING id", "oxygen", beehive1ID).Scan(&sensor4ID)
+	err = conn.QueryRow(context.Background(), "INSERT INTO sensors (type, beehive_id) VALUES ($1, $2) RETURNING id", "oxygen", beehive1ID).Scan(&sensor4ID)
     if err != nil {
         return fmt.Errorf("failed to insert sensor4: %v", err)
     }
-	err = dbpool.QueryRow(context.Background(), "INSERT INTO sensors (type, beehive_id) VALUES ($1, $2) RETURNING id", "temperature", beehive2ID).Scan(&sensor5ID)
+	err = conn.QueryRow(context.Background(), "INSERT INTO sensors (type, beehive_id) VALUES ($1, $2) RETURNING id", "temperature", beehive2ID).Scan(&sensor5ID)
     if err != nil {
         return fmt.Errorf("failed to insert sensor5: %v", err)
     }
-    err = dbpool.QueryRow(context.Background(), "INSERT INTO sensors (type, beehive_id) VALUES ($1, $2) RETURNING id", "humidity", beehive2ID).Scan(&sensor6ID)
+    err = conn.QueryRow(context.Background(), "INSERT INTO sensors (type, beehive_id) VALUES ($1, $2) RETURNING id", "humidity", beehive2ID).Scan(&sensor6ID)
     if err != nil {
         return fmt.Errorf("failed to insert sensor6: %v", err)
     }
-    err = dbpool.QueryRow(context.Background(), "INSERT INTO sensors (type, beehive_id) VALUES ($1, $2) RETURNING id", "weight", beehive2ID).Scan(&sensor7ID)
+    err = conn.QueryRow(context.Background(), "INSERT INTO sensors (type, beehive_id) VALUES ($1, $2) RETURNING id", "weight", beehive2ID).Scan(&sensor7ID)
     if err != nil {
         return fmt.Errorf("failed to insert sensor7: %v", err)
     }
-	err = dbpool.QueryRow(context.Background(), "INSERT INTO sensors (type, beehive_id) VALUES ($1, $2) RETURNING id", "oxygen", beehive2ID).Scan(&sensor8ID)
+	err = conn.QueryRow(context.Background(), "INSERT INTO sensors (type, beehive_id) VALUES ($1, $2) RETURNING id", "oxygen", beehive2ID).Scan(&sensor8ID)
     if err != nil {
         return fmt.Errorf("failed to insert sensor8: %v", err)
     }
+
+    // // Read JSON file
+    // jsonData, err := os.Open("/test/test_data.json")
+    // if err != nil {
+    //     log.Fatalf("Error opening json file, err: ", err)
+    // }
+    // defer jsonData.Close()
+
+    // var readings []models.SensorData
+    // decoder := json.NewDecoder(jsonData)
+    // err = decoder.Decode(&readings)
+
+    // if err != nil {
+    //     log.Fatalf("Error decoding JSON data: %v", err)
+    // }
+    
+  
+
+    // // Insert each sensor reading into the database
+    // for _, reading := range readings {
+    //     _, err := conn.Exec(context.Background(),
+    //         "INSERT INTO sensor_data (sensor_id, beehive_id, value, time) VALUES ($1, $2, $3, $4)",
+    //         reading.SensorID, reading.BeehiveID, reading.Value, reading.Time)
+    //     if err != nil {
+    //         log.Fatal("Failed to insert sensor data for sensor_id %d: %v", reading.SensorID, err)
+    //     }
+    // }
+    // log.Println("Data successfully inserted into the database")
 
     // Initialize the string slice with date-time values
     testTimes := []string{
@@ -127,7 +163,7 @@ func InjectTestData(dbpool *pgxpool.Pool) error {
             INSERT INTO sensor_data (sensor_id, beehive_id, value, time)
             VALUES ($1, $2, $3, $4)
         `
-        _, err := dbpool.Exec(context.Background(), insertSQL, data.SensorID, data.BeehiveID, data.Value, data.Time)
+        _, err := conn.Exec(context.Background(), insertSQL, data.SensorID, data.BeehiveID, data.Value, data.Time)
         if err != nil {
             return fmt.Errorf("failed to insert test sensor data: %v", err)
         }
