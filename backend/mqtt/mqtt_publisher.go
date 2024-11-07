6626ce37a1a548f7c8e223952a0a11fb8645a339
Bungo <alxber-0@student.ltu.se>
2024-11-07 12:46:59 +0100

    updated publisher logging and subscriber

--- backend/mqtt/mqtt_publisher.go
+++ backend/mqtt/mqtt_publisher.go
@@ -3,7 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
-	"math/rand"
+	// "math/rand"
 	"os"
 	"time"
 
@@ -14,7 +14,7 @@ func main() {
 	// Set MQTT broker URL
 	// broker := "tcp://localhost:1883" // If we are running our own local broker
 	// broker := "broker.emqx.io:1883" // emqx public broker
-	broker := "tcp://broker.hivemq.com:1883"   // HiveMQ public broker
+	broker := "broker.hivemq.com:1883" // HiveMQ public broker
 	topic := "d0039ebeehive/sensor"
 
 	opts := mqtt.NewClientOptions()
@@ -32,17 +32,57 @@ func main() {
 		os.Exit(1)
 	}
 
+	// JSON payload
+	sensorValue := `{
+		"applicationID":"1",
+		"applicationName":"beehive-sensor-card",
+		"data":"YWFhYWE=",
+		"devEUI":"0080e115000adf82",
+		"deviceName":"beehive-sensor-card-dn",
+		"fCnt":319,
+		"fPort":2,
+		"rxInfo":[{
+			"altitude":0,
+			"latitude":0,
+			"loRaSNR":7.5,
+			"longitude":0,
+			"mac":"24e124fffef0b4f9",
+			"name":"24e124fffef0b4f9",
+			"rssi":-109,
+			"time":"2024-11-05T14:01:49.217376Z"
+		}],
+		"time":"2024-11-05T14:01:49.217376Z",
+		"txInfo":{
+			"adr":true,
+			"codeRate":"4/5",
+			"dataRate":{
+				"bandwidth":125,
+				"modulation":"LORA",
+				"spreadFactor":7
+			},
+			"frequency":868300000
+		}
+	}`
+
 	// Publish float data to the topic at regular intervals
 	for {
-		// Generate a random float value
-		sensorValue := rand.Float64() * 100
-		payload := fmt.Sprintf("%f", sensorValue)
+		// // Generate a random float value
+		// sensorValue := rand.Float64() * 100
+		// payload := fmt.Sprintf("%f", sensorValue)
+		//
+		// // Publish the value
+		// token := client.Publish(topic, 0, false, payload)
+		// token.Wait()
+		//
+		// fmt.Printf("Published %s to topic %s\n", payload, topic)
+		//
+		// time.Sleep(5 * time.Second) // Send every 5 seconds
 
 		// Publish the value
-		token := client.Publish(topic, 0, false, payload)
+		token := client.Publish(topic, 0, false, sensorValue)
 		token.Wait()
 
-		fmt.Printf("Published %s to topic %s\n", payload, topic)
+		fmt.Printf("Published %s to topic %s\n", sensorValue, topic)
 
 		time.Sleep(5 * time.Second) // Send every 5 seconds
 	}
--- backend/mqtt/mqtt_subscriber.go
+++ backend/mqtt/mqtt_subscriber.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
@@ -11,6 +12,12 @@ import (
 	mqtt "github.com/eclipse/paho.mqtt.golang"
 )
 
+type SensorMessage struct {
+	ApplicationName string `json:"applicationName"`
+	Data            string `json:"data"`
+	Time            string `json:"time"`
+}
+
 // Initialize the log file
 var logFile *os.File
 
@@ -18,10 +25,40 @@ var messagePubHandler mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Me
 	// Get the current timestamp
 	timestamp := time.Now().Format("2006-01-02 15:04:05")
 
+	// Parse the received message payload
+	var sensorMessage SensorMessage
+	if err := json.Unmarshal(msg.Payload(), &sensorMessage); err != nil {
+		log.Printf("Error parsing JSON: %v", err)
+		return
+	}
+
+	// Parse the message as it is received like this:
+	// {
+	// "applicationID":"1",
+	// "applicationName":"beehive-sensor-card",
+	// "data":"YWFhYWE=",
+	// "devEUI":"0080e115000adf82",
+	// "deviceName":"beehive-sensor-card-dn",
+	// "fCnt":319,
+	// "fPort":2,
+	// "rxInfo":[{"altitude":0,"latitude":0,"loRaSNR":7.5,"longitude":0,"mac":"24e124fffef0b4f9","name":"24e124fffef0b4f9","rssi":-109,"time":"2024-11-05T14:01:49.217376Z"}],
+	// "time":"2024-11-05T14:01:49.217376Z",
+	// "txInfo":{"adr":true,"codeRate":"4/5","dataRate":{"bandwidth":125,"modulation":"LORA","spreadFactor":7},"frequency":868300000}
+	// }
+	logMessage := fmt.Sprintf("%s - applicationName: %s, data: %s, time: %s\n",
+		timestamp, sensorMessage.ApplicationName, sensorMessage.Data, sensorMessage.Time)
+
 	// Log the received message to the log file with timestamp
-	if _, err := logFile.WriteString(fmt.Sprintf("%s - Received message: %s from topic: %s\n", timestamp, msg.Payload(), msg.Topic())); err != nil {
+	// if _, err := logFile.WriteString(fmt.Sprintf("%s - Received message: %s from topic: %s\n", timestamp, msg.Payload(), msg.Topic())); err != nil {
+	// 	log.Printf("Error writing to log file: %v", err)
+	// }
+
+	// Log the parsed message to the log file
+	if _, err := logFile.WriteString(logMessage); err != nil {
 		log.Printf("Error writing to log file: %v", err)
 	}
+
+	fmt.Println(logMessage)
 }
 
 var connectHandler mqtt.OnConnectHandler = func(client mqtt.Client) {
@@ -36,7 +73,7 @@ func main() {
 	// Set MQTT broker URL
 	// broker := "tcp://localhost:1883" // For testing hosting the broker on a local machine
 	// broker := "broker.emqx.io:1883" // emqx public broker
-	broker := "tcp://broker.hivemq.com:1883"   // HiveMQ public broker
+	broker := "broker.hivemq.com:1883" // HiveMQ public broker
 	topic := "d0039ebeehive/sensor"
 
 	// Open log file
@@ -49,8 +86,7 @@ func main() {
 
 	opts := mqtt.NewClientOptions()
 	opts.AddBroker(broker)
-	opts.SetClientID("go_mqtt_subscriber")
-
+	opts.SetClientID("local_subscriber")
 	opts.SetDefaultPublishHandler(messagePubHandler)
 	opts.OnConnect = connectHandler
 	opts.OnConnectionLost = connectLostHandler
