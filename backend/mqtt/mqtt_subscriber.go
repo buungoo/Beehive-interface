97d781795053dfae9a4599ddc5975d986c1808c0
Bungo <alxber-0@student.ltu.se>
2024-11-17 21:38:28 +0100

    added parent macaddr to the sensor types

--- backend/mqtt/mqtt_subscriber.go
+++ backend/mqtt/mqtt_subscriber.go
@@ -3,21 +3,17 @@ package main
 import (
 	"beehive_api/utils"
 	"encoding/base64"
-	"strings"
-
-	// "encoding/binary"
-	// "encoding/hex"
 	"encoding/json"
 	"fmt"
 	"log"
+	"net"
 	"os"
 	"os/signal"
-
-	mqtt "github.com/eclipse/paho.mqtt.golang"
-
-	// "strings"
+	"strings"
 	"syscall"
 	"time"
+
+	mqtt "github.com/eclipse/paho.mqtt.golang"
 )
 
 // Sensor enum to represent sensor types
@@ -33,18 +29,20 @@ const (
 
 // SensorReading struct to hold a parsed sensor reading
 type SensorReading struct {
-	SensorType Sensor
-	SensorID   uint8
-	Value      interface{} // Allows for different types, like int8 or uint8
-	Timestamp  time.Time   // To store the timestamp of the reading
+	SensorType    Sensor
+	SensorID      uint8
+	Value         interface{}      // Allows for different types, like int8 or uint8
+	Timestamp     time.Time        // To store the timestamp of the reading
+	ParentBeehive net.HardwareAddr // MAC address of the parent Beehive
 }
 
 // Builder pattern for SensorReading
 type SensorReadingBuilder struct {
-	sensorType Sensor
-	sensorID   uint8
-	value      interface{}
-	timestamp  time.Time
+	sensorType    Sensor
+	sensorID      uint8
+	value         interface{}
+	timestamp     time.Time
+	parentBeehive net.HardwareAddr
 }
 
 func NewSensorReadingBuilder(sensorType Sensor, timestamp time.Time) *SensorReadingBuilder {
@@ -61,37 +59,43 @@ func (b *SensorReadingBuilder) SetValue(value interface{}) *SensorReadingBuilder
 		if v, ok := value.(int8); ok {
 			b.value = v
 		} else {
-			// log.Println("Received invalid value type for Temperature. Expected int8.")
 			utils.LogWarn("Received invalid value type for Temperature. Expected int8.")
 		}
 	case Microphone:
 		if v, ok := value.(uint8); ok {
 			b.value = v == 1 // Microphone can be either 0 or 1. We should assign a boolean value.
 		} else {
-			// log.Println("Invalid value type for Temperature. Expected int8.")
 			utils.LogWarn("Invalid value type for Temperature. Expected int8.")
 		}
 	default:
 		if v, ok := value.(uint8); ok {
 			b.value = v
 		} else {
-			// log.Println("Received invalid value type for default sensors. Expected uint8.")
 			utils.LogWarn("Received invalid value type for default sensors. Expected uint8.")
 		}
 	}
 	return b
 }
 
-func (b *SensorReadingBuilder) Build() *SensorReading {
-	return &SensorReading{
-		SensorType: b.sensorType,
-		SensorID:   b.sensorID,
-		Value:      b.value,
-		Timestamp:  b.timestamp,
+func (b *SensorReadingBuilder) SetDevEUI(parentBeehive string) *SensorReadingBuilder {
+	mac, err := net.ParseMAC(parentBeehive)
+	if err != nil {
+		utils.LogWarn("Invalid DevEUI format. Expected MAC address.")
+	} else {
+		b.parentBeehive = mac
 	}
+	return b
 }
 
-
+func (b *SensorReadingBuilder) Build() *SensorReading {
+	return &SensorReading{
+		SensorType:    b.sensorType,
+		SensorID:      b.sensorID,
+		Value:         b.value,
+		Timestamp:     b.timestamp,
+		ParentBeehive: b.parentBeehive,
+	}
+}
 
 func parseSensorMessage(message SensorMessage) ([]*SensorReading, error) {
 	decodedData, err := base64.StdEncoding.DecodeString(message.Data)
@@ -101,7 +105,6 @@ func parseSensorMessage(message SensorMessage) ([]*SensorReading, error) {
 
 	if dotIndex := strings.Index(message.Time, "."); dotIndex != -1 {
 		message.Time = message.Time[:dotIndex] + "Z" // Add "Z" to indicate UTC
-		// fmt.Println(message.Time)
 	}
 
 	timeStamp, err := time.Parse("2006-01-02T15:04:05Z", message.Time)
@@ -115,7 +118,7 @@ func parseSensorMessage(message SensorMessage) ([]*SensorReading, error) {
 		sensorId := decodedData[i+1]
 		rawValue := decodedData[i+2]
 
-		builder := NewSensorReadingBuilder(sensorType, timeStamp).SetSensorID(sensorId)
+		builder := NewSensorReadingBuilder(sensorType, timeStamp).SetSensorID(sensorId).SetDevEUI(message.DevEUI)
 		switch sensorType {
 		case Temperature:
 			builder.SetValue(int8(rawValue)) // Temperature uses int8
@@ -133,13 +136,11 @@ func parseSensorMessage(message SensorMessage) ([]*SensorReading, error) {
 func handleSensorMessage(message SensorMessage) {
 	readings, err := parseSensorMessage(message)
 	if err != nil {
-		// log.Printf("Error parsing sensor message: %v", err)
 		utils.LogError("Error parsing sensor message: %v", err)
 		return
 	}
 
 	for _, reading := range readings {
-		// fmt.Printf("Sensor Reading: %+v\n", reading)
 		utils.LogInfo(fmt.Sprint("Sensor Reading: %+v\n", reading))
 		// Parse the message into sensor objects
 	}
@@ -149,16 +150,8 @@ type SensorMessage struct {
 	ApplicationName string `json:"applicationName"`
 	Data            string `json:"data"`
 	Time            string `json:"time"`
+	DevEUI          string `json:"devEUI"`
 }
-
-// type Sensor uint8
-//
-// const (
-// 	LoadCell    Sensor = 1
-// 	Temperature        = 2
-// 	Humidity           = 3
-// 	Microphone         = 4
-// )
 
 // Initialize the log file
 var logFile *os.File
@@ -189,52 +182,11 @@ var messagePubHandler mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Me
 	// "txInfo":{"adr":true,"codeRate":"4/5","dataRate":{"bandwidth":125,"modulation":"LORA","spreadFactor":7},"frequency":868300000}
 	// }
 
-	// logMessage := fmt.Sprintf("%s - applicationName: %s, data: %s, time: %s",
-	// 	timestamp, sensorMessage.ApplicationName, sensorMessage.Data, sensorMessage.Time)
-	utils.LogInfo(fmt.Sprintf("Received message - applicationName: %s, data: %s, time: %s",
-		sensorMessage.ApplicationName, sensorMessage.Data, sensorMessage.Time))
-
-	// Log the received message to the log file with timestamp
-	// if _, err := logFile.WriteString(fmt.Sprintf("%s - Received message: %s from topic: %s\n", timestamp, msg.Payload(), msg.Topic())); err != nil {
-	// 	log.Printf("Error writing to log file: %v", err)
-	// }
-
-	// Log the parsed message to the log file
-	// if _, err := logFile.WriteString(logMessage); err != nil {
-	// 	log.Printf("Error writing to log file: %v", err)
-	// }
-	// utils.LogInfo(logMessage)
+	// utils.LogInfo(fmt.Sprintf("Received message - applicationName: %s, data: %s, time: %s",
+	// 	sensorMessage.ApplicationName, sensorMessage.Data, sensorMessage.Time))
+	utils.LogInfo(fmt.Sprintf("Received message: %+v", sensorMessage))
 
 	handleSensorMessage(sensorMessage) //.Data)
-
-	// h, err := base64.StdEncoding.DecodeString(sensorMessage.Data)
-	// if err != nil {
-	// 	// handle error
-	// }
-	//
-	// fmt.Println(h)
-
-	// sensorId byte := 0
-	// sensorType := 0
-	// sensorValue := 0
-	// sensorId, sensorType, sensorValue byte
-	// sensorId, sensorType, sensorValue := byte(0), byte(0), byte(0)
-	//
-	// for index, element := range h {
-	// 	fmt.Println("Index:", index, "Element:", element)
-	//
-	// 	switch {
-	// 	case index%3 == 1:
-	// 		sensorType = element
-	// 		fmt.Println("sensorType assigned:", sensorType)
-	// 	case index%3 == 2:
-	// 		sensorId = element
-	// 		fmt.Println("sensorId assigned:", sensorId)
-	// 	case index%3 == 0:
-	// 		sensorValue = element
-	// 		fmt.Println("sensorValue assigned:", sensorValue)
-	// 	}
-	// }
 }
 
 var connectHandler mqtt.OnConnectHandler = func(client mqtt.Client) {
@@ -242,53 +194,52 @@ var connectHandler mqtt.OnConnectHandler = func(client mqtt.Client) {
 }
 
 var connectLostHandler mqtt.ConnectionLostHandler = func(client mqtt.Client, err error) {
-	// fmt.Printf("Connection lost: %v\n", err)
 	utils.LogError("Connection lost", err)
 }
 
 func main() {
-    broker := "broker.hivemq.com:1883" // HiveMQ public broker
-    topic := "d0039ebeehive/sensor"
+	broker := "broker.hivemq.com:1883" // HiveMQ public broker
+	topic := "d0039ebeehive/sensor"
 
-    // Initialize the logger
-    logFile, err := utils.InitLogger("./logs/subscriberlog")
-    if err != nil {
-        utils.LogFatal("Failed to initialize logger", err)
-    }
-    defer logFile.Close() // Ensure the log file is closed when the program terminates
+	// Initialize the logger
+	logFile, err := utils.InitLogger("./logs/subscriberlog")
+	if err != nil {
+		utils.LogFatal("Failed to initialize logger", err)
+	}
+	defer logFile.Close() // Ensure the log file is closed when the program terminates
 
-    opts := mqtt.NewClientOptions()
-    opts.AddBroker(broker)
-    opts.SetClientID("local_subscriber")
-    opts.SetDefaultPublishHandler(messagePubHandler)
-    opts.OnConnect = connectHandler
-    opts.OnConnectionLost = connectLostHandler
+	opts := mqtt.NewClientOptions()
+	opts.AddBroker(broker)
+	opts.SetClientID("local_subscriber")
+	opts.SetDefaultPublishHandler(messagePubHandler)
+	opts.OnConnect = connectHandler
+	opts.OnConnectionLost = connectLostHandler
 
-    // Create client
-    client := mqtt.NewClient(opts)
-    if token := client.Connect(); token.Wait() && token.Error() != nil {
-        utils.LogFatal("Error connecting to broker", token.Error())
-    }
+	// Create client
+	client := mqtt.NewClient(opts)
+	if token := client.Connect(); token.Wait() && token.Error() != nil {
+		utils.LogFatal("Error connecting to broker", token.Error())
+	}
 
-    // Subscribe to the topic
-    if token := client.Subscribe(topic, 1, nil); token.Wait() && token.Error() != nil {
-        utils.LogFatal("Error subscribing to topic", token.Error())
-    }
+	// Subscribe to the topic
+	if token := client.Subscribe(topic, 1, nil); token.Wait() && token.Error() != nil {
+		utils.LogFatal("Error subscribing to topic", token.Error())
+	}
 
-    stopChan := make(chan os.Signal, 1)
-    signal.Notify(stopChan, syscall.SIGINT, syscall.SIGTERM)
+	stopChan := make(chan os.Signal, 1)
+	signal.Notify(stopChan, syscall.SIGINT, syscall.SIGTERM)
 
-    // Keep the subscriber running
-    go func() {
-        for {
-            time.Sleep(1 * time.Second)
-        }
-    }()
+	// Keep the subscriber running
+	go func() {
+		for {
+			time.Sleep(1 * time.Second)
+		}
+	}()
 
-    // Wait for shutdown signal
-    <-stopChan
+	// Wait for shutdown signal
+	<-stopChan
 
-    // Cleanup before exiting
-    client.Disconnect(250)
-    utils.LogInfo("Subscriber disconnected and exiting")
+	// Cleanup before exiting
+	client.Disconnect(250)
+	utils.LogInfo("Subscriber disconnected and exiting")
 }
